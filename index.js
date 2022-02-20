047507ca783d7cdf774d740cfcb76578138452f5
Douglas Silva <douglastotvs@gmail.com>
2022-02-20 16:50:49 -0300

    .

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 require("dotenv").config();
 const express = require("express");
-const connetTodb = require('./database/db')
+const connetTodb = require("./database/db");
+const patch = require("path");
+const Music = require("./model/Music");
 
 const app = express();
 const port = process.env.port || 3000;
 
+app.set("view engine", "ejs");
+app.use(express.static(patch.join(__dirname, "public")));
+app.use(express.urlencoded());
+
 connetTodb();
 
-app.get("/", (req, res) => {
-  res.send("Hello Word");
+app.get("/", async (req, res) => {
+  const playlist = await Music.find();
+
+  res.render("index", { playlist });
+});
+
+app.get("/admin", (req, res) => {
+  res.render("admin");
+});
+
+app.post("/create", async (req, res) => {
+  const music = req.body;
+  await Music.create(music);
+  res.redirect("/");
 });
 
 app.listen(
